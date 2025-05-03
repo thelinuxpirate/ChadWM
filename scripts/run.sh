90cbd804fa6d78d9387f8878ac40ea1465ee34af
trong <spektralfrogadier@gmail.com>
2025-05-03 12:09:50 -0700

    Update Scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,11 +5,12 @@ xset s off &
 xset s noblank &
 xset -dpms &
 xrdb merge ~/.Xresources &
-feh --bg-fill ~/Pictures/Wallpapers/landscape/pink_clouds.png &
+emacs --daemon &
+feh --bg-fill ~/Pictures/Wallpapers/default.png &
 dash ~/.config/chadwm/scripts/bar.sh &
 picom &
 flameshot &
 dunst &
-playme -t ~/.local/audio/SMG2-CosmicCoveGalaxy.mp3 -d 1 &
+# playmectl -t ~/.local/audio/SMG2-CosmicCoveGalaxy.mp3
 
 while type chadwm >/dev/null; do chadwm && continue || break; done
